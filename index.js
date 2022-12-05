d1338a3484ea9460ce27ab858fe9268fefe0d521
Ali Omar <ali.noor.student.moringaschool.com git config --gobal Ali abdullahigit config --global user.email ali.noor.student.moringaschool.com>
2022-12-05 12:40:02 +0300

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "Ali", 
+    streetAddress: "New York",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return{
+        ...obj,
+        [key]: value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const deletedEmployee = {...obj};
+    delete deletedEmployee[key];
+    return deletedEmployee; 
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj;
+}
